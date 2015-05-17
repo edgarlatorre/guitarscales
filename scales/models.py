438a7b01d848506e29875735ea06c8440f699407
Edgar <me@edgarlatorre.com.br>
2015-05-17 23:17:51 +0100

    added __str__ to scales models

--- scales/models.py
+++ scales/models.py
@@ -4,10 +4,16 @@ class Scale(models.Model):
     key = models.CharField(max_length=2)
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return u"%s %s" % (self.key, self.name)
+
 class Shape(models.Model):
     number = models.IntegerField()
     first_fret = models.IntegerField()
     scale = models.ForeignKey(Scale)
+
+    def __str__(self):
+        return u"%s %s" % (str(self.scale), self.number)
 
 class Position(models.Model):
     fret = models.IntegerField()
@@ -15,3 +21,7 @@ class Position(models.Model):
     finger = models.IntegerField()
     is_root = models.BooleanField()
     shape = models.ForeignKey(Shape)
+
+    def __str__(self):
+        return u"%s Position: %d : %d" % (str(self.shape),
+            self.fret, self.string)
--- scales/tests.py
+++ scales/tests.py
@@ -1,3 +1,55 @@
 from django.test import TestCase
+from scales.models import Scale, Shape, Position
 
-# Create your tests here.
+class ScaleTest(TestCase):
+    def test_scale__representation(self):
+        scale = Scale()
+        scale.key = "E"
+        scale.name = "Minor Pentatonic"
+
+        self.assertEquals("%s %s" % (scale.key, scale.name), str(scale))
+
+class ShapeTest(TestCase):
+    def setUp(self):
+        self.scale = Scale()
+        self.scale.key = "E"
+        self.scale.name = "Minor Pentatonic"
+        self.scale.save()
+
+    def tearDown(self):
+        Scale.objects.all().delete()
+
+    def test_shape_string_representation(self):
+        shape = Shape()
+        shape.number = 1
+        shape.scale = self.scale
+
+        self.assertEquals("%s %s" % (str(self.scale), shape.number), str(shape))
+
+class PositionTest(TestCase):
+    def setUp(self):
+        self.scale = Scale()
+        self.scale.key = "E"
+        self.scale.name = "Minor Pentatonic"
+        self.scale.save()
+
+        self.shape = Shape()
+        self.shape.number = 1
+        self.shape.first_fret = 1
+        self.shape.scale = self.scale
+        self.shape.save()
+
+    def tearDown(self):
+        Scale.objects.all().delete()
+        Shape.objects.all().delete()
+
+    def test_shape_string_representation(self):
+        position = Position()
+        position.fret = 1
+        position.string = 2
+        position.shape = self.shape
+
+        expected = "%s Position: %d : %d" % (str(self.shape),
+            position.fret, position.string)
+
+        self.assertEquals(expected, str(position))
