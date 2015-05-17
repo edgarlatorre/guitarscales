320a8b8fa272970d24a32cf160eaf1f2078deeb3
Edgar <me@edgarlatorre.com.br>
2015-05-17 01:36:02 +0100

    created app pages and index page

---
+++ pages/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.test import Client
+
+class IndexPage(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_return_200(self):
+        response = self.client.get('/')
+        self.assertEquals(200, response.status_code)
+
+    def test_index_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
