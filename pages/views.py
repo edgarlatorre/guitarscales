191b7f39497d645879554772b007c0630d03c297
Edgar <me@edgarlatorre.com.br>
2015-06-23 23:02:38 +0100

    added select with the scales options

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,9 @@
 from django.views.generic import TemplateView
+from scales.models import Scale
 
 class IndexView(TemplateView):
     template_name = "index.html"
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context["scales"] = Scale.objects.all()
+        return self.render_to_response(context)
